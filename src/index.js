ba6aa2272ed80efd8fe705060331883ba49da026
bowei <bowei.han100@gmail.com>
2018-08-21 18:31:07 -0400

    export types from package

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 
 import { Tree, TreeNode } from './components';
 
+export type {
+  TreeProps,
+  TreeNodeProps,
+  Node,
+  Theme,
+  CheckboxProps,
+  BodyProps,
+  ExpanderProps,
+  ListItemProps,
+  ListProps,
+  LoadingProps,
+  DepthPaddingProps,
+} from './types';
+
 export { Tree, TreeNode };
+
 export default Tree;
